8cd68b1964386055213a9dcc441c93d15630aa5b
briangraj <brian.graj@gmail.com>
2020-01-30 10:15:27 -0300

    agrego destacado pero con dudas

--- alumnoCRUD/alumnoController.js
+++ alumnoCRUD/alumnoController.js
@@ -1,9 +1,16 @@
 const Curso = require('../models/Curso');
 
 const getAlumnos = (req, res, next) => {
+  let message
+  if (req.query.destacado === "s") {
+    message = req.curso.alumnos.sort((alumno1, alumno2) => alumno2.nota - alumno1.nota)[0]
+  } else {
+    message = req.curso.alumnos
+  }
+
   res.status(200).json({
     code: 0,
-    message: req.curso.alumnos
+    message: message
   });
 };
 
--- models/schemas/Alumno.js
+++ models/schemas/Alumno.js
@@ -4,7 +4,8 @@ const Alumno = new mongoose.Schema({
   nombre: { type: String },
   apellido: { type: String },
   dni: { type: Number },
-  direccion: { type: String }
+  direccion: { type: String },
+  nota: { type: Number }
 });
 
 module.exports = Alumno;
