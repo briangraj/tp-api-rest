2bca4b7f0cb6f6f96a9a780986cb3ca4e33d4b31
briangraj <brian.graj@gmail.com>
2020-02-01 20:26:42 -0300

    agrego logout

--- usuariosCRUD/usuariosController.js
+++ usuariosCRUD/usuariosController.js
@@ -26,7 +26,7 @@ const postLogin = (req, res) => {
     .then(user => {
       user.generateAuthToken()
         .then(token => {
-          res.json({
+          Status.ok(res, {
             usuario: user,
             token: token
           })
@@ -38,4 +38,24 @@ const postLogin = (req, res) => {
     })
 }
 
-module.exports = { postUsuario, postLogin }
+const getMiUsuario = (req, res) => {
+  Status.ok(res, req.user)
+}
+
+const postLogout = (req, res, next) => {
+  req.user.tokens = req.user.tokens.filter((token) => {
+    return token.token != req.token
+  })
+  req.user.save()
+    .then(() => { Status.ok(res, "El logout de esta sesion se completo correctamente")})
+    .catch(next)
+}
+
+const postLogoutAll = (req, res, next) => {
+  req.user.tokens.splice(0, req.user.tokens.length)
+  req.user.save()
+    .then(() => Status.ok(res, "El logout de todas las sesiones se completo correctamente"))
+    .catch(next)
+}
+
+module.exports = { postUsuario, postLogin, getMiUsuario, postLogout, postLogoutAll }
--- usuariosCRUD/usuariosRouter.js
+++ usuariosCRUD/usuariosRouter.js
@@ -1,9 +1,13 @@
 const usuariosRouter = require('express').Router()
 
-const { postUsuario, postLogin } = require('./usuariosController');
+const { postUsuario, postLogin, getMiUsuario, postLogout, postLogoutAll } = require('./usuariosController');
+const auth = require('./usuariosValidator');
 
 usuariosRouter.post('/', postUsuario)
-
 usuariosRouter.post('/login', postLogin)
+
+usuariosRouter.get('/me', auth, getMiUsuario)
+usuariosRouter.post('/me/logout', auth, postLogout)
+usuariosRouter.post('/me/logoutall', auth, postLogoutAll)
 
 module.exports = usuariosRouter
