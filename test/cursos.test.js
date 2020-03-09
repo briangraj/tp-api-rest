f9d6be4c441320759f0e5f9f0937451e6ce4d34d
briangraj <brian.graj@gmail.com>
2020-03-09 16:52:55 -0300

    sigo con tests

--- test/cursos.test.js
+++ test/cursos.test.js
@@ -1,12 +1,29 @@
+require('dotenv').config();
 const app = require('../app');
 const supertest = require('supertest');
 const request = supertest(app);
-require('dotenv').config();
+const mongoose = require('mongoose');
+const Usuario = require('../models/Usuario');
+
+const body = {
+  'Accept': 'application/json',
+  'Authorization': process.env.TOKEN
+};
+const databaseName = 'test'
+
+beforeAll(async () => {
+  const url = `mongodb://localhost:27017/${databaseName}`
+  await mongoose.connect(url, { useNewUrlParser: true })
+})
+
+afterEach(async () => {
+  await Usuario.deleteMany()
+})
 
 describe('Test cursos', function() {
   describe('Al buscar todos los cursos', function() {
     it('deberia retornar 200 y message deberia ser un array', async done => {
-      const response = await request.get('/cursos').set('Accept', 'application/json').set('Authorization', process.env.TOKEN);
+      const response = await request.get('/cursos').set(body);
 
       expect(response.status).toBe(200);
       expect(response.body.message).toBeInstanceOf(Array);
@@ -14,3 +31,17 @@ describe('Test cursos', function() {
     })
   })
 });
+
+it('Should save user to database', async done => {
+  const res = await request.post('/usuarios')
+    .send({
+      username: 'Zell',
+      password: 'testing@gmail.com'
+    })
+
+  const user = await Usuario.findOne({ username: 'Zell' })
+  expect(user.username).toBeTruthy()
+  expect(user.password).toBeTruthy()
+
+  done()
+})
